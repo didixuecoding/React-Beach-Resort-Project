203308dea4aa9848bac530ce5948f6889b21d419
LearnToWin <60451278+didixuecoding@users.noreply.github.com>
2020-04-06 05:02:53 -0400

    Lec96
    Using consumer or high order component to pass context

---
+++ src/components/RoomFilter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RoomFilter = () => {
+  return <div>Hello From RoomFilter</div>;
+};
+
+export default RoomFilter;
---
+++ src/components/RoomList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RoomList = () => {
+  return <div>Hello From RoomList</div>;
+};
+
+export default RoomList;
