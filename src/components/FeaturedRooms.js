ba11839e9081d4533651e491f62d4d0d33ad7d4d
LearnToWin <60451278+didixuecoding@users.noreply.github.com>
2020-04-05 20:04:01 -0400

    Lec89
    Featured Room Component

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,13 +1,26 @@
 import React from "react";
+
 import { RoomContext } from "../context";
+import Loading from "./Loading";
+import Room from "./Room";
+import Title from "./Title";
 
 class FeaturedRooms extends React.Component {
   static contextType = RoomContext;
   render() {
-    const { featuredRooms } = this.context;
-    console.log(featuredRooms);
+    let { loading, featuredRooms } = this.context,
+      rooms = featuredRooms.map(room => {
+        return <Room key={room.id} room={room} />;
+      });
 
-    return <div>Hello from FeaturedRooms</div>;
+    return (
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
+    );
   }
 }
 
