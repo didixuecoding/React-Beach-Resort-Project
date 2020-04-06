8d24865e4ce8673abbc1787788162ed2d5eb43bb
LearnToWin <60451278+didixuecoding@users.noreply.github.com>
2020-04-06 01:47:49 -0400

    Lec92,93
    Single Room Component
    Part I

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,8 +1,48 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import { RoomContext } from "../context";
+import defaultBcg from "../../public/images/room-1.jpeg";
+import Hero from "../components/Hero";
+import Banner from "../components/Banner";
 
 class SingleRoom extends React.Component {
+  state = { slug: this.props.match.params.slug, defaultBcg };
+  static contextType = RoomContext;
   render() {
-    return <div>Hello from Single Room</div>;
+    const { getRoom } = this.context;
+    const room = getRoom(this.state.slug);
+    if (!room) {
+      return (
+        <div className="error">
+          <h3>no such room could be found</h3>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </div>
+      );
+    }
+    const {
+      name,
+      description,
+      capacity,
+      size,
+      price,
+      extras,
+      breakfast,
+      pets,
+      images
+    } = room;
+
+    return (
+      <Hero hero="roomsHero">
+        <Banner title={`${name} room`}>
+          <Link to="/rooms" className="btn-primary">
+            back to rooms
+          </Link>
+        </Banner>
+      </Hero>
+    );
   }
 }
 
